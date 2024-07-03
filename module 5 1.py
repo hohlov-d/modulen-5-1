5fe08641201799977449880a06aa50c0b5d1776f
hohlov-d <hohlov_d.o@mail.ru>
2024-07-03 21:16:31 +0500

    Add files via upload

---
+++ module 5 1.py
@@ -0,0 +1,18 @@
+class House:
+    def __init__(self, name, number_of_flours):
+        self.name = name
+        self.number_of_floors = number_of_flours
+    def go_to(self, new_floor):
+        self.new_floor = new_floor
+        for i in range(1, self.new_floor + 1):
+            print(i)
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print('Такого этажа не существует')
+
+
+
+h1 = House('ЖК Ботаника', 20)
+h2 = House('Бирюзовая', 5)
+h1.go_to(0)
+h2.go_to(6)
+
